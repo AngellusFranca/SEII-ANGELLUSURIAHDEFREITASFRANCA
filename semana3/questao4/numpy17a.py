5271cd8de02b803389a653eb40c1a45f05022661
AngellusFranca <68348880+AngellusFranca@users.noreply.github.com>
2022-10-24 20:12:30 -0300

    Update numpy17a.py

--- semana3/questao4/numpy17a.py
+++ semana3/questao4/numpy17a.py
@@ -3,9 +3,11 @@ import numpy as np
 a = np.array([[1,2], [3,4]])
 eigenvalues, eigenvectors = np.linalg.eig(a)
 
+
 print(eigenvalues)
 print(eigenvectors) 
 print(eigenvectors[:,0])
+
 
 d = eigenvectors[:,0] * eigenvalues[0]
 e = a @ eigenvectors[:, 0]
